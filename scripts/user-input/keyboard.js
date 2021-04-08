b77c70d7ae81500de5f019db970be835f8c98f82
Kristina Gergel <gergel-kristina@mail.ru>
2021-04-08 14:54:23 +0300

    ref #1 first approximation done

--- scripts/user-input/keyboard.js
+++ scripts/user-input/keyboard.js
@@ -20,6 +20,35 @@ function reactToKeyboardInput (event) {
 
     // sanitizeCannonState
 
+    function barrelOfOfScreen () {
+        const farthermostBarrelPointX = cannonX + cannonWidth * Math.cos(cannonAngle)
+        const farthermostBarrelPointY = cannonY - cannonWidth * Math.sin(cannonAngle)
+        return (farthermostBarrelPointX < 0) ||
+            (farthermostBarrelPointX > canvas.width) ||
+            (farthermostBarrelPointY < 0) ||
+            (farthermostBarrelPointY > canvas.height)
+    }
+
+    // sanitizeCannonAngle
+    if (barrelOfOfScreen()) {
+        switch (event.key) {
+            case 'ArrowLeft':
+                cannonAngle -= 0.1
+                break
+            case 'ArrowRight':
+                cannonAngle += 0.1
+                break
+            case 'ArrowUp':
+                cannonX -= cannonV * Math.cos(cannonAngle)
+                cannonY += cannonV * Math.sin(cannonAngle)
+                break
+            case 'ArrowDown':
+                cannonX += cannonV * Math.cos(cannonAngle)
+                cannonY -= cannonV * Math.sin(cannonAngle)
+                break
+        }
+    }
+
     // don't allow movement beyond the edge of the screen
     // sanitizeCannonPosition
     if (cannonX < cannonRadius) {
