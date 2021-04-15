dd27948a354dcedd15f54d3cea45b3e9fd7bc3e0
Kristina Gergel <gergel-kristina@mail.ru>
2021-04-15 14:32:18 +0300

    ref #1 allow barrel to reach to edge of the screen when rotating

--- scripts/user-input/keyboard.js
+++ scripts/user-input/keyboard.js
@@ -20,24 +20,57 @@ function reactToKeyboardInput (event) {
 
     // sanitizeCannonState
 
+    // sanitizeCannonAngle
+    const farthermostBarrelPointX = cannonX + cannonWidth * Math.cos(cannonAngle)
+    const farthermostBarrelPointY = cannonY - cannonWidth * Math.sin(cannonAngle)
+
     function barrelOfOfScreen () {
-        const farthermostBarrelPointX = cannonX + cannonWidth * Math.cos(cannonAngle)
-        const farthermostBarrelPointY = cannonY - cannonWidth * Math.sin(cannonAngle)
         return (farthermostBarrelPointX < 0) ||
             (farthermostBarrelPointX > canvas.width) ||
             (farthermostBarrelPointY < 0) ||
             (farthermostBarrelPointY > canvas.height)
     }
 
-    // sanitizeCannonAngle
     if (barrelOfOfScreen()) {
         switch (event.key) {
             case 'ArrowLeft':
-                cannonAngle -= 0.1
-                break
             case 'ArrowRight':
-                cannonAngle += 0.1
+                
+                /*
+                Arc-functions change sign weirdly depending on the particular edge
+                and the clock- or counter-clock-wise movement. BEWARE!
+                */
+                
+                if (farthermostBarrelPointX < 0) {
+                    cannonAngle = Math.acos((0 - cannonX) / cannonWidth)
+                    if (event.key === 'ArrowRight') {
+                        cannonAngle = -cannonAngle
+                    }
+                }
+
+                if (farthermostBarrelPointX > canvas.width) {
+                    cannonAngle = Math.acos((canvas.width - cannonX) / cannonWidth)
+                    if (event.key === 'ArrowLeft') {
+                        cannonAngle = -cannonAngle
+                    }
+                }
+                
+                if (farthermostBarrelPointY < 0) {
+                    cannonAngle = Math.asin((cannonY - 0) / cannonWidth)
+                    if (event.key === 'ArrowRight') {
+                        cannonAngle = Math.PI - cannonAngle
+                    }
+                }
+
+                if (farthermostBarrelPointY > canvas.height) {
+                    cannonAngle = Math.asin((cannonY - canvas.height) / cannonWidth)
+                    if (event.key === 'ArrowLeft') {
+                        cannonAngle = Math.PI - cannonAngle
+                    }
+                }
+                
                 break
+
             case 'ArrowUp':
                 cannonX -= cannonV * Math.cos(cannonAngle)
                 cannonY += cannonV * Math.sin(cannonAngle)
